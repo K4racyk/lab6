967953a1026a4a0a2fb78a398bf44901d267bfcc
K4racyk <94282655+K4racyk@users.noreply.github.com>
2022-02-04 20:32:00 +0100

    Add files via upload

---
+++ Zadania Lab6/Zadania Lab6/Zadania Lab6.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream> 
+using namespace std;
+
+//------------Zadanie 1 ---------------------
+//int main() {
+//
+//	int* ptr, * ptr1, * ptr2, a = 700, b = 255, c = 8;
+//	
+//	
+//	
+//	ptr = &a;
+//	ptr1 = &b;
+//	ptr2 = &c;
+//	
+//	
+//	
+//	
+//	cout << "roznica: " << (*ptr) - (*ptr1) << endl;
+//	cout << "srednia: " << (double)((*ptr) + (*ptr1) + (*ptr2)) / 3 << endl;
+//	cout << "adres: " << ptr << endl;
+//	cout << "wartosc: " << *ptr << endl;
+//}
+//-------------------------Zadanie 2 -------------------------------------
+//int main()
+//{
+//	int n, k,p;
+//	cout << "Podaj rozmiar tablicy: ";
+//	cin >> n;
+//	cout << "Podaj zakres tablicy: " << endl;
+//	cin >> p;
+//	cout << "Podaj koniec tablicy: " << endl;
+//	cin >> k;
+//
+//
+//	if (n>0)
+//	{
+//		if (p <= k)
+//		{
+//			int* tab = new int[n];
+//			for (int i = 0; i < n; i++)
+//			{
+//				if(p<0)
+//				tab[i] = rand() % (p + k + 1) - p;
+//				if (p > 0)
+//					tab[i] = rand() % (p + k + 1) + p;
+//				if (p == 0)
+//					tab[i] = rand() % (p + k + 1);
+//				cout << "Wylosowane elementy: " << tab[i] << " Adres: " << &tab[i] << endl;
+//				
+//			}
+//		}
+//		else {
+//			cout << "Podano zly przedzial" << endl;
+//		}
+//	}
+//}
